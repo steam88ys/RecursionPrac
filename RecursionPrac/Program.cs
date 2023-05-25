69496cbc2dc51a70416075f7a312fc2e95d3d703
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-05-25 14:42:13 +0900

    초기 커밋.

---
+++ RecursionPrac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fino = new Fibonacci();
+            Console.WriteLine(fibo.Get(10));
+        }
+    }
+}
